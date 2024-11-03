afb74e58ee7be1f57b1339e3e659609c9b5c0dcc
lahgit <jesse.lahti@helsinki.fi>
2024-11-03 19:02:39 +0200

    tehtava 8

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,6 +10,11 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_alku_saldo_negatiivinen(self):
+        varasto2 = Varasto(10, -5)
+        self.assertAlmostEqual(varasto2.saldo, 0)
+        
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
@@ -31,6 +36,29 @@ class TestVarasto(unittest.TestCase):
 
         self.assertAlmostEqual(saatu_maara, 2)
 
+    def test_str(self):
+        varasto10 = Varasto(10,5)
+        expected_str = "saldo = 5, vielä tilaa 5"
+        self.assertEqual(str(varasto10), expected_str)
+
+    def test_negative_creation(self):
+        varasto9 = Varasto(10,20)
+        self.assertEqual(varasto9.saldo, 10)
+
+    def test_negative_add(self):
+        varasto8 = Varasto(10,5)
+        varasto8.lisaa_varastoon(-1)
+        self.assertEqual(varasto8.saldo, 5)
+
+    def test_overflow_add(self):
+        varasto5 = Varasto(10,5)
+        varasto5.lisaa_varastoon(20)
+        self.assertEqual(varasto5.saldo, 10)
+
+    def test_none_creation(self):
+        varasto7 = Varasto(0,0)
+        self.assertEqual(varasto7.tilavuus, 0)
+
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +66,19 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+
+    def test_negatiivinen_ottaminen(self):
+        varasto5 = Varasto(10,5)
+        varasto5.ota_varastosta(-1)
+
+        # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
+        self.assertAlmostEqual(varasto5.saldo, 5)
+
+
+    def test_ottaminen(self):
+        varasto3 = Varasto(10,5)
+        varasto3.ota_varastosta(6)
+
+        # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
+        self.assertAlmostEqual(varasto3.saldo, 0)
